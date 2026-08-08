4417b853a76e0c968d4f20107c38c7e304d124fc
rsf1091shunsuke-source <rsf1091shunsuke@gmail.com>
2026-08-08 14:44:24 +0900

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import StudyTracker from "@/components/StudyTracker";
-
-// このページは常にクライアント側で完結するダッシュボードなので、
-// ビルド時にFirebaseへ接続しようとして失敗しないよう静的プリレンダリングを無効化する。
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <StudyTracker />;
-}
